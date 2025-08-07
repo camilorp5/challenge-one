ea06aaa3ea1b6b9983338fa67441ea8cddd4f48d
Camilo Rafael Pérez Chaves <camilorafaelperezchaves@gmail.com>
2025-08-07 17:27:54 -0500

    comenzando el codigo js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+lista_amigos = [];
+
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo');
+    if  (nombreAmigo.value == '') {
+        alert('Ingrese un valor válido')
+    } else {
+        lista_amigos.push(nombreAmigo.value);
+        console.log(lista_amigos)
+    }
+}
+
